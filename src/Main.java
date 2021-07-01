9aa42071ff259a3dbcbf380456fdf19afdf8da99
vitalianoCosta <84536802+vitalianoCosta@users.noreply.github.com>
2021-07-01 12:18:34 +0100

    Party name definition to create file

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Party party1 = new Party("Party1", 5);
+        Party party1 = new Party("Gryffindor Alumni", 5);
         Party party2 = new Party("Party2", 5);
-        ImportExport.writePartyToFile("Party1.csv", party1.getAliveCharacters());
+        ImportExport.writePartyToFile(party1.getName().replace(" ", "_"), party1.getAliveCharacters());
 
 
         Battle battle1 = new Battle(party1, party2);
