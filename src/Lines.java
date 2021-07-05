b25de48b07aa58ae578c67596b5606d9e429229e
Katarzyna Wasik <kat.wasik@hotmail.com>
2021-07-05 17:31:30 +0200

    Add Lines class to aid menu design

---
+++ src/Lines.java
@@ -0,0 +1,15 @@
+public class Lines {
+    public static void printEmpty(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+        }
+    }
+
+    public static void printUpper() {
+        System.out.println("================================================================");
+    }
+
+    public static void printGameName() {
+        System.out.println("MUGGLES AGAINST MAGIC\nby TheCleanCoders");
+    }
+}
